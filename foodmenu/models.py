c5f8fe689e0c2167a2b4599621b25855bb1a3cac
Carlos Garcia <chenzhen.dev@gmail.com>
2023-05-22 16:55:41 -0300

    feat(model): definindo modelagem inicial

--- foodmenu/models.py
+++ foodmenu/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name =  models.CharField(max_length=45, null=False, blank=False)
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class Dish(models.Model):
+    name =  models.CharField(max_length=45, null=False, blank=False)
+    image =  models.CharField(max_length=100, blank=False)
+    description =  models.TextField(null=False, blank=False)
+    price = models.DecimalField(max_digits=8, decimal_places=2, null=False)
+    category =  models.ForeignKey(Category, on_delete=models.PROTECT)
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+    
+class ingredient(models.Model):
+    name =  models.CharField(max_length=45, null=False, blank=False)
+    dish =  models.ForeignKey(Dish, on_delete=models.PROTECT, default=None, null=True)
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
