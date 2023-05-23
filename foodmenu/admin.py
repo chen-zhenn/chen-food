2cae6317be2a660f2d637673bde0d142f5065686
Carlos Garcia <chenzhen.dev@gmail.com>
2023-05-23 10:44:12 -0300

    feat(admin): definindo setup básico

--- foodmenu/admin.py
+++ foodmenu/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from foodmenu.models import Category, Culinary, Ingredient, Dish
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "deleted"]
+    list_display_links = ["name"]
+
+admin.site.register(Category, CategoryAdmin)
+
+class CulinaryAdmin(admin.ModelAdmin):
+    list_display = ["name", "deleted"]
+    list_display_links = ["name"]
+
+admin.site.register(Culinary, CulinaryAdmin)
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ["description", "deleted"]
+    list_display_links = ["description", "deleted"]
+
+admin.site.register(Ingredient, IngredientAdmin)
+
+class DishAdmin(admin.ModelAdmin):
+    list_display = ["name", "image", "description", "price", "category", "culinary", "ingredient", "deleted"]
+    list_display_links = ["name", "image", "description", "price", "category", "culinary", "ingredient"]
+    
+admin.site.register(Dish, DishAdmin)
