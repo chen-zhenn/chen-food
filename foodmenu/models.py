54687f93e9a1e91a0a7608f7832ee8a1169f0b89
Carlos Garcia <chenzhen.dev@gmail.com>
2023-05-23 10:17:20 -0300

    feat(model): alterando relacionamento e campos das entidades Ingredient e Dish

--- foodmenu/models.py
+++ foodmenu/models.py
@@ -1,21 +1,24 @@
 from django.db import models
 
 # Create your models here.
-
 class Category(models.Model):
     name =  models.CharField(max_length=45, null=False, blank=False)
     deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
-
 class Culinary(models.Model):
     name =  models.CharField(max_length=45, null=False, blank=False)
     deleted = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.name
+        return self.name  
+class Ingredient(models.Model):
+    description =  models.TextField(null=False, blank=False)
+    deleted = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
 class Dish(models.Model):
     name =  models.CharField(max_length=45, null=False, blank=False)
     image =  models.CharField(max_length=100, blank=False)
@@ -23,14 +26,7 @@ class Dish(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, null=False)
     category =  models.ForeignKey(Category, on_delete=models.PROTECT)
     culinary =  models.ForeignKey(Culinary, on_delete=models.PROTECT, default=None, null=True)
-    deleted = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.name
-    
-class Ingredient(models.Model):
-    name =  models.CharField(max_length=45, null=False, blank=False)
-    dish =  models.ForeignKey(Dish, on_delete=models.PROTECT, default=None, null=True)
+    ingredient =  models.ForeignKey(Ingredient, on_delete=models.PROTECT, default=None, null=True)
     deleted = models.BooleanField(default=False)
 
     def __str__(self):
