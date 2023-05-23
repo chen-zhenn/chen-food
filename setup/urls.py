15cc2c890529c7e9a92f55594e7a500de3392ef3
Carlos Garcia <chenzhen.dev@gmail.com>
2023-05-23 19:13:23 -0300

    feat(routes): adicionando rota para setviews

--- setup/urls.py
+++ setup/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from foodmenu.views import CategoryViewSet, CulinaryViewSet, IngredientViewSet, DishViewSet
+
+router = routers.DefaultRouter()
+router.register("category", CategoryViewSet)
+router.register("culinary", CulinaryViewSet)
+router.register("ingredient", IngredientViewSet)
+router.register("dish", DishViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('foodmenu.urls')),
+    path('', include(router.urls)),
 ]
