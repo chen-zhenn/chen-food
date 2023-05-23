74867361e62a6845e8c42f7f74db550c34973f9b
Carlos Garcia <chenzhen.dev@gmail.com>
2023-05-23 19:11:36 -0300

    feat(view): adicionando setviews

--- foodmenu/views.py
+++ foodmenu/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+from foodmenu.serializers import CategorySerializer, CulinarySerializer, IngredientSerializer, DishSerializer
+from foodmenu.models import Category, Culinary, Ingredient, Dish
 
 # Create your views here.
 def index(req):
     return render(req, 'index.html')
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CulinaryViewSet(viewsets.ModelViewSet):
+    queryset = Culinary.objects.all()
+    serializer_class = CulinarySerializer
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+class DishViewSet(viewsets.ModelViewSet):
+    queryset = Dish.objects.all()
+    serializer_class = DishSerializer
